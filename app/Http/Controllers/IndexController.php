f62075dfbde8e59aa7f1b428a10d3bf619a16b69
qujingwen <qujingwen@Huawenholding.com>
2021-02-26 15:14:47 +0800

    修改

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Redis;
+
+class IndexController extends BaseController
+{
+    public function index(){
+
+    }
+}
